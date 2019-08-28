0639aa5e1fbbe0cae4e3c720d36721afb47176f3
jessi ortega <ortegabx05@gmail.com>
2019-08-28 22:28:25 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -23,9 +23,19 @@ class Game
      board.turn_count.odd? ? player_2 : player_1
   end
 
-
+  def won?
+    WIN_COMBINATIONS.each do |combination|   
+        if @board.cells[combination[0]] == @board.cells[combination[1]] &&
+          @board.cells[combination[1]] == @board.cells[combination[2]] &&
+          @board.taken?(combination[0]+1)
+          return combination
+        end
+      end
+    return false
+  end
 
   def draw?
     @board.full? && !won? ? true : false
   end
+  
 end
