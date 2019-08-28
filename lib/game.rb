3033789368a14e7cae0293d2b6ab01ca937e4e99
jessi ortega <ortegabx05@gmail.com>
2019-08-28 22:42:32 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,11 +61,5 @@ class Game
     @board.display
   end
   
-    def play
-    turn until over?
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
+  
 end
